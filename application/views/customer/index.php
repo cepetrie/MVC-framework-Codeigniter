2d9ed670ed617c6d8552b8fb0ca3f8001d28c75b
cepetrie <christinapetrie@hotmail.com>
2015-04-30 18:05:49 -0700

    Added customer.php and associated files

---
+++ application/views/customer/index.php
@@ -0,0 +1,29 @@
+<?php 
+
+/**
+ * index.php view page for generic Customer controller
+ * 
+ * Used to show how to do CRUD in CodeIgniter
+ *
+ * views/customer/index.php
+ *
+ * @package ITC260
+ * @subpackage Customer
+ * @author Christina Petrie <ChristinaPetrie@hotmail.com>
+ * @version 1.0 2015/04/30 
+ * @link http://www.example.com/ 
+ * @license http://www.apache.org/licenses/LICENSE-2.0
+ * @see Customer_model.php
+ * @see Customer.php
+ * @todo none
+ */
+
+//views/news/index.php
+$this->load->view($this->config->item('theme') . 'header');
+?>
+
+<h2><?php echo $title ?></h2>
+
+<?php 
+$this->load->view($this->config->item('theme') . 'footer');
+?>
